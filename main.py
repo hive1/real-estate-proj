8f9aaf08ab0ac8837d4a63fa9e152cdcf7ec2d55
James Sears <93846353+hive1@users.noreply.github.com>
2024-04-07 11:24:58 -0400

    more gui stuff
    resized the first window, now starting to implement some cosmetic changes and introduction to the second gui

--- main.py
+++ main.py
@@ -2,32 +2,30 @@ from tkinter import *
 
 def main():
 
-    WIDTH = 1024
-    LENGTH = 500
+    WIDTH = 512
+    LENGTH = 250
 
     # Creating a root window for main operations
     root = Tk()
 
     # root window 
-    root.title("Input Data")
+    root.title("Real Estate Collector v1")
     root.geometry(f'{WIDTH}x{LENGTH}')
 
-    # useless quit button
-    Button(root, text='leave!!', command=root.quit).place(relx=.05, rely=.05, anchor=NW)
-
     # main code
-    Label(root, text = 'Please input either the postal code or the name of desired city: ', font=('Kannada MW', 17)).place(relx=.5, rely=.05, anchor=N)
+    Label(root, text = 'Please input the name or zip code of your desired location: ', font=('Kannada MW', 17)).place(relx=.5, rely=.05, anchor=N)
 
     def displayData(textarg):
         Label(root, text=textarg).grid()
 
     # The following variable would be used for storing the post code or name of city that the user requests
     locationVar = StringVar(root, value = None)
-    Entry(root, textvariable=locationVar).grid()
+    Entry(root, textvariable=locationVar).place(relx=0.3, rely=0.18)
     
-
-    # submitButton = Button(root, text='submit', command=lambda: displayData(locationVar.get()))
-    # submitButton.grid()
+    submitButton = Button(root, text='SUBMIT')
+    submitButton.configure(command=lambda: displayData(locationVar.get()))
+    submitButton.configure(relief=SUNKEN, padx=1.5, pady=1.5, fg='black', font=('Kannada MW', '12'))
+    submitButton.place(relx=0.4, rely=0.3)
 
     root.mainloop()
 
