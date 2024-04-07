9d825984b2b716558559fd42116a6c3bd8a149b3
James Sears <93846353+hive1@users.noreply.github.com>
2024-04-07 14:02:36 -0400

    ttk
    style!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import * 
+from tkinter.ttk import Style
 
 def main():
 
@@ -17,17 +18,21 @@ def main():
 
     basicFrame = Frame(root).pack()
     inputLabel = Label(basicFrame, text = 'Please input the name or zip code of your desired location: ', font=('Kannada MW', 17))
-    inputLabel.pack(side=TOP)
+    inputLabel.pack(side=TOP, pady=5)
 
     # The following variable would be used for storing the post code or name of city that the user requests
     locationVar = StringVar(basicFrame, value = None)
-    Entry(basicFrame, textvariable=locationVar).pack()
-    
+    Entry(basicFrame, textvariable=locationVar).pack(pady=10)
+
+    # Creating a style variable
+    style = Style()
+    style.configure('W.TButton', font = ('calibri', 10, 'bold', 'underline'), foreground = 'red')
+
     # goofy submit button
     submitButton = Button(basicFrame, text='SUBMIT')
     submitButton.configure(command=lambda: displayData(locationVar.get()))
     submitButton.configure(relief=SUNKEN, padx=1.5, pady=1.5, font=('Kannada MW', '12'))
-    submitButton.pack()
+    submitButton.pack(pady=10)
 
     root.mainloop()
 
