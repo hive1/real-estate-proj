39247f4a55af1e16342a7fbf435493201aedfa46
jjmecca <46037675+jjmecca@users.noreply.github.com>
2024-04-15 13:35:55 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -9,30 +9,35 @@ import json
 
 # https://sites.google.com/chromium.org/driver/
 
-zip_code=input("Enter zip code: ")
+zip=input("Enter zip code: ")
 
 driver = webdriver.Chrome()
 driver.get("https://www.redfin.com")
 
-button=driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div[2]/div/section/div/div/div/div/div/div/div/div[2]/div/div/form/div/div/input')
-button.click()
-button.send_keys(zip_code)
-button.send_keys(Keys.ENTER)
-total=0
-counter=0
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "bp-Homecard__Content"))
-)
-for element in driver.find_elements(By.CLASS_NAME, "bp-Homecard__Content"):
-    price= element.find_element(By.CSS_SELECTOR,"span.bp-Homecard__Price--value").text
-    address=element.find_element(By.CSS_SELECTOR, "div.bp-Homecard__Address.flex.align-center.color-text-primary.font-body-xsmall-compact").text
-    bed=element.find_element(By.CSS_SELECTOR, "span.bp-Homecard__Stats--beds.text-nowrap").text
-    bath=element.find_element(By.CSS_SELECTOR, "span.bp-Homecard__Stats--baths.text-nowrap").text
-    print(price,"\n",bed,bath,"\n",address,"\n")
-    price=price.replace(",","")
-    price=int(price.replace("$",""))
-    total+=price
-    counter+=1
-print("$",(total/counter))
+def get_info(zip):
+    button=driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div[2]/div/section/div/div/div/div/div/div/div/div[2]/div/div/form/div/div/input')
+    button.click()
+    button.send_keys(zip)
+    button.send_keys(Keys.ENTER)
+    total=0
+    counter=0
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "bp-Homecard__Content"))
+    )
+    for element in driver.find_elements(By.CLASS_NAME, "bp-Homecard__Content"):
+        price= element.find_element(By.CSS_SELECTOR,"span.bp-Homecard__Price--value").text
+        address=element.find_element(By.CSS_SELECTOR, "div.bp-Homecard__Address.flex.align-center.color-text-primary.font-body-xsmall-compact").text
+        bed=element.find_element(By.CSS_SELECTOR, "span.bp-Homecard__Stats--beds.text-nowrap").text
+        bath=element.find_element(By.CSS_SELECTOR, "span.bp-Homecard__Stats--baths.text-nowrap").text
+        print(price,"\n",bed,bath,"\n",address,"\n")
+        price=price.replace(",","")
+        price=int(price.replace("$",""))
+        total+=price
+        counter+=1
+def get_average():
+    print("$",(total/counter))
+
+get_info(zip)
+
 time.sleep(5)
 driver.quit
