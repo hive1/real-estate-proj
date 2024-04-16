8a60bfe9fda6c8bc4735c10b6f132de6fb7b7e94
jjmecca <46037675+jjmecca@users.noreply.github.com>
2024-04-16 16:11:48 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,18 +3,40 @@ from tkinter.ttk import Style
 root = Tk()
 list=[0,1,2,3,4,5]
 i=0
-element=Label(text=list[i])
+main_frame = Frame(root)
+element=Label(main_frame,text=list[i])
 element.pack()
+main_frame.pack()
 
 root.geometry('600x500')
 root.title('Real Estate Data')
 
 def next():
-    element.forget()
+    global i
+    global element
     i+=1
-    element=Label(text=list[i])
+    if i>=(len(list)-1):
+        next_button.forget()
+        pass
+    main_frame.forget()
+    element.forget()
+    element=Label(main_frame,text=list[i])
+    main_frame.pack()
+    element.pack()
+def back():
+    global i
+    global element
+    i-=1
+    if i<=0:
+        back_button.forget()
+        pass
+    main_frame.forget()
+    element.forget()
+    element=Label(main_frame,text=list[i])
+    main_frame.pack()
     element.pack()
 next_button=Button(text="Next",command=next)
+back_button=Button(text="Back",command=back)
 next_button.pack()
-
+back_button.pack()
 root.mainloop()
