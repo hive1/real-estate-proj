f90920eb2ce592545256f4f4fbe0f9c5084be3db
jjmecca <46037675+jjmecca@users.noreply.github.com>
2024-04-17 15:31:36 -0400

    removing headless

--- scraping.py
+++ scraping.py
@@ -18,21 +18,11 @@ def scrapeData(zip_code):
 
     '''Yea but imagine if we didn't have to SEE selenium work'''
     chrome_options = Options()
-    chrome_options.add_argument("--disable-gpu")
-    '''chrome_options.add_argument("--headless=new")  # Ensure GUI is off
-    chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-    '''
+    chrome_options.add_argument("--disable-gpu") # Disables GPU hardware acceleration
+    #chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+    #chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://www.redfin.com")
-    # chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
-    # chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-
-    ''' Yea but imagine if we didn't have to SEE selenium work
-    chrome_options = ChromeOptions()
-    chrome_options.add_argument("--headless")  # Enables headless mode
-    chrome_options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
-    '''
 
     button=driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div[2]/div/section/div/div/div/div/div/div/div/div[2]/div/div/form/div/div/input')
     button.click()
